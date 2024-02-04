39fca8fdae4b85e2d0af0091eedade60f6b571ee
mitantsooa <99263685+mitantsooa@users.noreply.github.com>
2024-02-04 21:49:21 +0300

    change
    change

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var server = require("./server");
 
-const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://nyainamitantsoa1:w74du1b6ijbmI2QB@projectbs.wmabkvh.mongodb.net/?retryWrites=true&w=majority");
-const User = require('./models/userModels');
 
-server.start();
+//server.start();
