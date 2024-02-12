c43743126ee74b692e7ba89b15146952490ab788
TanjonaRak <99284907+TanjonaRak@users.noreply.github.com>
2024-02-12 22:51:42 +0100

    gestion employee

--- Controllers/Employee/EmployeeController.js
+++ Controllers/Employee/EmployeeController.js
@@ -1,5 +1,6 @@
 
 
+const { getClient } = require('../../Utility/db');
 const EmployeeModel = require('../../models/Employe/Employee');
 
 class EmployeeController {
@@ -17,19 +18,33 @@ class EmployeeController {
     }
 
     async getEmployee (req,res){
+        let client = null;
         try {
             let offset = req.params._offset;
             let limit = req.params._limit
-            const data = await EmployeeModel.getEmployee(null,offset,limit);
-            res.status(200).send({data,status:200,message:"request success"});
+            client = await getClient();
+            let db = client.db(process.env.DB_NAME); 
+            const data = await EmployeeModel.getEmployee(db,offset,limit);
+            const lineNumber = await EmployeeModel.getLigneNumber(db,limit);
+            res.status(200).send({data,status:200,message:"request success",lineNumber:lineNumber});
+        } catch (error) {
+            res.status(500).send({message:error.message,status:500})
+        }finally{
+            if (client!=null) {
+                client.close();
+            }
+        }
+    }
+
+    async UpdateEmployee(req,res){  
+        try {
+            // let _id = req.params._id
+            let emp_update = {...req.body};
+            const data_update = await EmployeeModel.UpdateEmployee(emp_update,null);
+            res.status(200).send({data_update,status:200,message:"request success"});
         } catch (error) {
             res.status(500).send({message:error.message,status:500})
         }
-
-    }
-
-    async UpdateEmployee(res,req){
-
     }
 } 
 
--- models/Employe/Employee.js
+++ models/Employe/Employee.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
 const { getClient } = require('../../Utility/db');
 require('dotenv').config();
+const { ObjectId } = require('mongoose').Types;
 
 class Employee {
 
@@ -12,7 +13,10 @@ class Employee {
         email :{type:String , required :true},
         password : {type:String , required :true},
         etat : {type:Number},
-        service : {type : Array }
+        service : {type : Array },
+        time_between : {type : String},
+        end_time : {type :String},
+        date_create : {type:Date},
     })
     EmployeModel = mongoose.model('employee',this.employee);
 
@@ -35,6 +39,7 @@ class Employee {
         try {
             console.log(employee)
             const newEmp = new this.EmployeModel(employee);
+            newEmp.date_create = new Date();
             const employee_Save = await newEmp.save();
             return employee_Save;
         } catch (error) {
@@ -45,15 +50,15 @@ class Employee {
     async getEmployee(db,offset,limit){
         let client = null;
         let db_test = 0;
-        console.log(offset)
-        console.log(limit)
+        // console.log(offset)
+        // console.log(limit)
         try {
             if(db == null){
                 db_test = 1;
                 client = await getClient();
                 db = client.db(process.env.DB_NAME); 
             }
-            let result = await db.collection('employees').find({}).skip(Number(offset)).limit(Number(limit)).toArray();
+            let result = await db.collection('employees').find({}).sort({_id:-1}).skip(Number(offset)).limit(Number(limit)).toArray();
             return result;
         } catch (error) {
             throw error;
@@ -64,11 +69,49 @@ class Employee {
         }
     }
 
-    async UpdateEmployee (employee,_id){
+    async getLigneNumber(db,limit){
         try {
-            
+            let result = await db.collection('employees').find({}).toArray();
+            return parseInt(((result.length/limit) + 1));
         } catch (error) {
-            
+            throw error;
+        }
+    }
+
+    async UpdateEmployee (employee,id,db){
+        let client = null;
+        let db_test = 0;
+
+        try {
+            const newEmp = new this.EmployeModel(employee);
+            // let result = newEmp.save();
+            if(db == null){
+                db_test = 1;
+                client = await getClient();
+                db = client.db(process.env.DB_NAME); 
+            }    
+            let result  = await db.collection('employees').updateOne(
+            {_id :new ObjectId(employee._id)},
+            {$set:{
+                name:newEmp.name,
+                first_name : newEmp.first_name,
+                email : newEmp.email,
+                password : newEmp.password,
+                login : newEmp.login,
+                service : newEmp.service,
+                date_last_update : new Date(),
+                time_between : newEmp.time_between,
+                end_time : newEmp.end_time
+                
+            }});
+            return result;
+
+        } catch (error) {
+            throw error;
+        }finally {
+            if (db_test != 0 && client!=null) {
+                client.close();
+            }
         }
     }
 
