07d18ca8e967e3692002b920f11fd89f82a72999
TanjonaRak <99284907+TanjonaRak@users.noreply.github.com>
2024-02-14 19:18:35 +0100

    service gestion

--- Route/index.js
+++ Route/index.js
@@ -8,8 +8,10 @@ const router = express.Router();
 module.exports = function (io) {
     const HistoriqueScanRoute = require('./HistoriqueScanRoute')(io);
     const EmployeeRoute  = require('./Employee/EmployeeRoute')(io);
+    const ServiceRoute = require('./Manager/ServiceRoute')(io);
 
     router.use('/scan',HistoriqueScanRoute);
     router.use('/employee',EmployeeRoute);
+    router.use('/service',ServiceRoute);
     return router;
 };
--- models/Manager/Service.js
+++ models/Manager/Service.js
@@ -1,6 +1,7 @@
 const { Schema } = require('mongoose');
 const mongoose = require('mongoose');
-
+const { getClient } = require('../../Utility/db');
+require('dotenv').config();
 class Service {
     service = new Schema({
         name : {type:String,required:true},
@@ -9,6 +10,7 @@ class Service {
         commission : {type:Number,required:true},
         description : {type:String},
         date_create : {type:Date},
+        state : {type:Number}
     })
     ServiceModel = mongoose.model('service',this.service);
 
@@ -16,11 +18,43 @@ class Service {
         try {
             const newService = new this.ServiceModel(service);
             newService.date_create = new Date();
+            newService.state = 10;
             const serviceCreate =await newService.save();
             return serviceCreate;
         } catch (error) {
             throw error;
         }        
+    }
+
+    async getService (db,offset,limit){
+        let client = null;
+        let db_test = 0;
+        // console.log(offset)
+        // console.log(limit)
+        try {
+            if(db == null){
+                db_test = 1;
+                client = await getClient();
+                db = client.db(process.env.DB_NAME); 
+            }
+            let result = await db.collection('services').find({}).sort({_id:-1}).skip(Number(offset)).limit(Number(limit)).toArray();
+            return result;
+        } catch (error) {
+            throw error;
+        }finally {
+            if (db_test != 0 && client!=null) {
+                client.close();
+            }
+        }
+    }
+
+    async getLigneNumber(db,limit){
+        try {
+            let result = await db.collection('services').find({}).toArray();
+            return parseInt(((result.length/limit) + 1));
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
